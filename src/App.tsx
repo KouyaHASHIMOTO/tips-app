573ef3592ed5f92dd2fce015d9046440f9814f14
KouyaHASHIMOTO <terubou821@gmail.com>
2026-05-21 17:00:39 +0900

    feat: Tailwind CSS、Vitest、Testing Libraryを追加

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className="app">
+        <h1 className='text-sky-500'>App</h1>
+      </div>
+    </>
+  )
+} 
+
+export default App

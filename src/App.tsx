6392fcc3b5556244fd3a2d1ae8dee30a7098acc4
KouyaHASHIMOTO <terubou821@gmail.com>
2026-05-28 16:06:30 +0900

    feat: App.tsxにHomePageを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,7 @@
-import { useState } from 'react'
-
-import './App.css'
+import { HomePage } from "./pages/home/HomePage";
 
 function App() {
-  const [count, setCount] = useState(0)
+  return <HomePage />;
+}
 
-  return (
-    <>
-    <div className="app">
-        <h1 className='text-sky-500'>App</h1>
-      </div>
-    </>
-  )
-} 
-
-export default App
+export default App;

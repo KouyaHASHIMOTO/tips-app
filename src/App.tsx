db71817ba1ccda5917bd683df5da86dd16b7f1dc
KouyaHASHIMOTO <terubou821@gmail.com>
2026-05-30 15:48:05 +0900

    feat:react-router-domで各遷移先を実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router";
 import { HomePage } from "./pages/home/HomePage";
+import { LoginPage } from "./pages/login/LoginPage";
+import { SignUpPage } from "./pages/signup/SignUpPage";
 
 function App() {
-  return <HomePage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;

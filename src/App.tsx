d9575a020e90ee3f1f4c220defb87bfbbd7c8bd6
KouyaHASHIMOTO <terubou821@gmail.com>
2026-06-01 21:59:37 +0900

    wip: ログイン状態の管理を実装中

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,21 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import { HomePage } from "./pages/home/HomePage";
 import { LoginPage } from "./pages/login/LoginPage";
 import { SignUpPage } from "./pages/signup/SignUpPage";
+import { useEffect, useState } from "react";
+import { supabase } from "./lib/supabase";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const load = async () => {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+      setUser(user);
+    };
+    load();
+  }, []);
   return (
     <BrowserRouter>
       <Routes>

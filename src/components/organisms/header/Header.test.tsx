64ca80733e2d7871442b91bcf423e0d20a96f0c1
KouyaHASHIMOTO <terubou821@gmail.com>
2026-06-02 22:21:05 +0900

    feat:ログアウト機能を実装

--- src/components/organisms/header/Header.test.tsx
+++ src/components/organisms/header/Header.test.tsx
@@ -1,9 +1,25 @@
 import { render, screen } from "@testing-library/react";
 import { Header } from "./Header";
+import { MemoryRouter } from "react-router-dom";
 
 describe("Header", () => {
   test("HeaderにTipsと表示されている", () => {
-    render(<Header />);
+    render(
+      <MemoryRouter>
+        <Header />
+      </MemoryRouter>
+    );
     expect(screen.getByText("Tips")).toBeInTheDocument();
+  });
+  test("Headerにログアウトボタンが有る", () => {
+    render(
+      <MemoryRouter>
+        <Header />
+      </MemoryRouter>
+    );
+
+    expect(
+      screen.getByRole("button", { name: "ログアウト" })
+    ).toBeInTheDocument();
   });
 });
--- src/components/organisms/header/Header.tsx
+++ src/components/organisms/header/Header.tsx
@@ -1,7 +1,23 @@
+import { useNavigate } from "react-router-dom";
+import { supabase } from "../../../lib/supabase";
+import { Button } from "../../atoms/button/Button";
+
 export const Header = () => {
+  const navigate = useNavigate();
+
+  const signOut = async () => {
+    const { error } = await supabase.auth.signOut();
+
+    if (error) {
+      console.error(error);
+      return;
+    }
+    navigate("/login");
+  };
   return (
-    <header className="fixed top-0 left-0 right-0 h-14 bg-white border-b border-gray-200 flex items-center px-4 z-50">
+    <header className="fixed top-0 left-0 right-0 h-14 bg-white border-b border-gray-200 flex items-center justify-between px-4 z-50">
       <h1 className="text-xl font-bold text-blue-500">Tips</h1>
+      <Button onClick={signOut}>ログアウト</Button>
     </header>
   );
 };

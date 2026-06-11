562d530fe4be3a7f62825cb3a8aaf65b7a2e0467
KouyaHASHIMOTO <terubou821@gmail.com>
2026-06-11 17:24:43 +0900

    test: Avatarにsrcがない場合のイニシャル表示テストを追加

--- src/components/atoms/avatar/Avatar.test.tsx
+++ src/components/atoms/avatar/Avatar.test.tsx
@@ -2,9 +2,14 @@ import { render, screen } from "@testing-library/react";
 import { Avatar } from "./Avatar";
 
 describe("Avatar", () => {
-  test("alt属性が正しく設定されている", () => {
+  test("alt属性がある場合は画像が表示される", () => {
     render(<Avatar alt="ユーザーアイコン" src="test-image.jpg" />);
     expect(screen.getByRole("img")).toHaveAttribute("alt", "ユーザーアイコン");
+  });
+
+  test("srcがない場合イニシャルが表示される", () => {
+    render(<Avatar alt="test" src="" />);
+    expect(screen.getByText("T")).toBeInTheDocument();
   });
 
   test("src属性が正しく設定されている", () => {

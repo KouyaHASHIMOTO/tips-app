1cd34b8ad0c57b07c8c0c45b85d1cba13bb50654
KouyaHASHIMOTO <terubou821@gmail.com>
2026-06-19 14:22:26 +0900

    wip:TipDetailPageを実装中

--- src/pages/tipdetailpage/TipDetailPage.tsx
+++ src/pages/tipdetailpage/TipDetailPage.tsx
@@ -1,3 +1,50 @@
+import { useEffect, useState } from "react";
+import { supabase } from "../../lib/supabase";
+import { useParams } from "react-router-dom";
+
 export const TipDetailPage = () => {
+  const [tip, setTip] = useState<{
+    id: number;
+    title: string;
+    user_id: string;
+    content: string;
+    created_at: string;
+    more_tips: {
+      id: number;
+      tip_id: number;
+      user_id: string;
+      content: string;
+      created_at: string;
+    }[];
+    profiles: {
+      id: number;
+      user_id: string;
+      user_name: string;
+      avatar_url: string;
+      created_at: string;
+    } | null;
+  } | null>(null);
+
+  const { id } = useParams();
+  useEffect(() => {
+    const fetchTip = async () => {
+      const { data, error } = await supabase
+        .from("tips")
+        .select(`*,more_tips (*),profiles (*)`)
+        .eq("id", id)
+        .single();
+
+      console.log(data);
+
+      if (error) {
+        console.error(error);
+        return;
+      }
+
+      setTip(data);
+    };
+    fetchTip();
+  }, [id]);
+
   return <div></div>;
 };

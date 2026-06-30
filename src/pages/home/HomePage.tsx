aafdab8feb91fb71d75bcc74db031052d60bfb35
KouyaHASHIMOTO <terubou821@gmail.com>
2026-06-30 23:00:03 +0900

    feat: 投稿時にトレンドタグも更新

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -56,7 +56,7 @@ export const HomePage = ({ user }: HomePageProps) => {
   const [isPostModalOpen, setIsPostModalOpen] = useState(false);
 
   const [trendTags, setTrendTags] = useState<{ name: string; count: number }[]>(
-    [],
+    []
   );
 
   const fetchTrendTags = async () => {
@@ -69,15 +69,12 @@ export const HomePage = ({ user }: HomePageProps) => {
 
     if (!data) return;
 
-    const countMap = data.reduce(
-      (acc, current) => {
-        const name = current.tags?.name;
-        if (!name) return acc;
-        acc[name] = (acc[name] ?? 0) + 1;
-        return acc;
-      },
-      {} as Record<string, number>,
-    );
+    const countMap = data.reduce((acc, current) => {
+      const name = current.tags?.name;
+      if (!name) return acc;
+      acc[name] = (acc[name] ?? 0) + 1;
+      return acc;
+    }, {} as Record<string, number>);
 
     // countMap を「件数が多い順に並んだタグの配列」に変換する
     const trendTags = Object.entries(countMap)
@@ -114,7 +111,7 @@ export const HomePage = ({ user }: HomePageProps) => {
     title: string,
     content: string,
     category: Category,
-    tags: string[],
+    tags: string[]
   ) => {
     // まずTipを保存
     const { data: tip, error } = await supabase
@@ -148,6 +145,7 @@ export const HomePage = ({ user }: HomePageProps) => {
     }
     setIsPostModalOpen(false);
     await fetchTips();
+    await fetchTrendTags();
   };
 
   const addLike = async (tipId: number, isLiked: boolean) => {
@@ -190,7 +188,7 @@ export const HomePage = ({ user }: HomePageProps) => {
     tagSearch !== ""
       ? tips
           .filter((tip) =>
-            tip.tip_tags.some((tt) => tt.tags?.name.includes(tagSearch)),
+            tip.tip_tags.some((tt) => tt.tags?.name.includes(tagSearch))
           )
           .filter((tip) => {
             if (!selectedCategory) {
@@ -200,8 +198,8 @@ export const HomePage = ({ user }: HomePageProps) => {
             }
           })
       : selectedCategory !== null
-        ? tips.filter((tip) => tip.category === selectedCategory)
-        : tips;
+      ? tips.filter((tip) => tip.category === selectedCategory)
+      : tips;
   return (
     <MainLayout
       rightPanel={
